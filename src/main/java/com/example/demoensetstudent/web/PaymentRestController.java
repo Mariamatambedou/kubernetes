889202362a10d387a2563cf7fd9c5f1090440c09
tambedou <tambedou89mariama@gmail.com>
2024-11-15 15:05:16 +0000

    stress

--- src/main/java/com/example/demoensetstudent/web/PaymentRestController.java
+++ src/main/java/com/example/demoensetstudent/web/PaymentRestController.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api")
 public class PaymentRestController {
     private StudentRepository studentRepository;
     private PaymentRepository paymentRepository;
