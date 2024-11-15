a2e0d5ef040088b821323b4153804f64204cac33
tambedou <tambedou89mariama@gmail.com>
2024-11-15 14:42:42 +0000

    the last commit now

--- frontend-angular/src/app/payments/payments.component.ts
+++ frontend-angular/src/app/payments/payments.component.ts
@@ -22,7 +22,7 @@ export class PaymentsComponent implements OnInit{
   constructor(private http: HttpClient,private router: Router){}
 
   ngOnInit() {
-    this.http.get('k8s.local/api/payments')
+    this.http.get('/api/payments')
     .subscribe({
       next : data => {
          this.payments = data;
