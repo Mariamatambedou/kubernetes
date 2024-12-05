3b7bd1e7a368cd304eed56a2cf0810c27b6319a7
tambedou <tambedou89mariama@gmail.com>
2024-12-05 07:25:00 +0000

    fichier value

--- src/main/java/com/example/demoensetstudent/DemoEnsetStudentApplication.java
+++ src/main/java/com/example/demoensetstudent/DemoEnsetStudentApplication.java
@@ -20,56 +20,7 @@ public class DemoEnsetStudentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoEnsetStudentApplication.class, args);
 	}
-@Bean
-	CommandLineRunner commandLineRunner(StudentRepository studentRepository, PaymentRepository paymentRepository) {
-		return args -> {
-			// Création de nouveaux étudiants avec UUID pour l'ID
-			Student student1 = Student.builder()
-					.id(UUID.randomUUID().toString())  // Générer un ID unique
-					.firstName("John")
-					.lastName("Doe")
-					.code("11100")
-					.programId("program1")
-					.photo("photo1.jpg")
-					.build();
 
-			Student student2 = Student.builder()
-					.id(UUID.randomUUID().toString())  // Générer un ID unique
-					.firstName("Jane")
-					.lastName("Doe")
-					.code("22200")
-					.programId("program2")
-					.photo("photo2.jpg")
-					.build();
-
-			// Sauvegarde des étudiants dans la base de données
-			studentRepository.save(student1);
-			studentRepository.save(student2);
-
-			// Création des paiements associés aux étudiants
-			Payment payment1 = Payment.builder()
-					.date(LocalDate.of(2024, 11, 1))
-					.amount(100.0)
-					.type(PaymentType.CASH)  // Valeur de type d'énumération
-					.status(PaymentStatus.VALIDATED)  // Valeur d'énumération
-					.file("receipt1.pdf")
-					.student(student1)  // Lier l'étudiant 1
-					.build();
-
-			Payment payment2 = Payment.builder()
-					.date(LocalDate.of(2024, 11, 2))
-					.amount(150.0)
-					.type(PaymentType.TRANSFER)
-					.status(PaymentStatus.CREATED)
-					.file("receipt2.pdf")
-					.student(student2)  // Lier l'étudiant 2
-					.build();
-
-			// Sauvegarde des paiements dans la base de données
-			paymentRepository.save(payment1);
-			paymentRepository.save(payment2);
-		};
 	}
 	
-}
 
